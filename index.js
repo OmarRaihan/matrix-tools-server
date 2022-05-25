6c9f202107dae99057e72c90f8a82513df0a1a13
OmarRaihan <omarraihan91@gmail.com>
2022-05-25 21:47:05 +0600

    create verifyJWT

--- index.js
+++ index.js
@@ -15,6 +15,23 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// VerifyJWT || Token
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "UnAuthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
+// Main Function
 async function run() {
   try {
     await client.connect();
@@ -57,9 +74,8 @@ async function run() {
     });
 
     // GET API || MyOrders by Email
-    app.get("/order/:email", async (req, res) => {
+    app.get("/order/:email", verifyJWT, async (req, res) => {
       const email = req.params.email;
-      const query = { email: email };
       const orders = await orderCollection.find(query).toArray();
       res.send(orders);
     });
